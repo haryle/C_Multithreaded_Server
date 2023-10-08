52356de171e9047fdc346fe449ed7084ba5c7393
hson <lehoangsonsg@gmail.com>
2023-10-08 20:19:42 +1030

    feat: implement list

---
+++ utils/list.c
@@ -0,0 +1,60 @@
+#include "list.h"
+
+void Concurrent_List_Init(list_t* L) {
+    L->map = (map_t*)malloc(sizeof(map_t));
+    Map_Init(L->map);
+    L->head = L->tail = NULL;
+    L->size = 0;
+    pthread_mutex_init(&L->tail_lock, NULL);
+}
+
+int Concurrent_List_Insert(list_t* L, char* title, char* value) {
+    int status = Map_Insert(L->map, title, value);
+    if (status == 1)
+        return status;
+    linked_list_t* list = Map_Get(L->map, title);
+    node_t* tail = List_Tail(list);
+    pthread_mutex_lock(&L->tail_lock);
+    L->size += 1;
+    // Only happen when the list is full
+    if (L->head == NULL) {
+        L->head = tail;
+    }
+    if (L->tail == NULL) {
+        L->tail = tail;
+    } else {
+        L->tail->next = tail;
+        L->tail = tail;
+    }
+    pthread_mutex_lock(&L->tail_lock);
+    return 0;
+}
+
+void Concurrent_List_Free(list_t* L) {
+    Map_Free(L->map);
+    free(L->map);
+}
+
+linked_list_t* Concurrent_List_Get_Book(list_t* L, char* title) {
+    return Map_Get(L->map, title);
+}
+
+bool Concurrent_List_Contains_Inefficient(list_t* L, char* title, char* value) {
+    pthread_mutex_lock(&L->tail_lock);
+    node_t* current = L->head;
+    while (current != NULL) {
+        if ((strcmp(current->title, title) == 0) &&
+            (strcmp(current->value, value) == 0)) {
+            pthread_mutex_unlock(&L->tail_lock);
+            return true;
+        }
+        current = current->next;
+    }
+    pthread_mutex_unlock(&L->tail_lock);
+    return false;
+}
+
+bool Concurrent_List_Contains(list_t* L, char* title, char* value) {
+    linked_list_t* list = Map_Get(L->map, title);
+    return List_Contains(list, title, value);
+}
---
+++ utils/list.h
@@ -0,0 +1,75 @@
+#include <pthread.h>
+#include "map.h"
+
+/*
+Thread-safe list data structure 
+
+Arguments: 
+head: ptr to the head of the list 
+tail: ptr to the tail of the list 
+map: ptr to the map data 
+tail_lock: lock
+size: list size
+*/
+typedef struct __list_t {
+    node_t* head;
+    node_t* tail;
+    map_t* map;
+    pthread_mutex_t tail_lock;
+    int size;
+} list_t;
+
+/*
+Initialise the list data structure
+
+Arguments:
+L - pointer to the list data structure to init value
+*/
+void Concurrent_List_Init(list_t* L);
+
+/*
+Insert a new node to the list data structure 
+
+Arguments: 
+L - ptr to the list data structure 
+title - string title of the book 
+value - string content of the book 
+*/
+int Concurrent_List_Insert(list_t* L, char* title, char* value);
+
+/*
+Destroy the given list
+*/
+void Concurrent_List_Free(list_t* L);
+
+/*
+Get book based on title 
+*/
+linked_list_t* Concurrent_List_Get_Book(list_t* L, char* title);
+
+/*
+Traverse the list via next. Very inefficient and is only used for testing 
+For practical use, try Concurrent_List_Contains method instead. 
+
+Arguments:
+L - pointer to the list to search
+title - string book title 
+value - string book content 
+
+Returns:
+true if there is a node with those values
+*/
+bool Concurrent_List_Contains_Inefficient(list_t*L, char* title, char* value);
+
+/*
+Efficient List contains search
+
+Arguments:
+L - pointer to the list to search
+title - string book title 
+value - string book content 
+
+Returns:
+true if there is a node with those values
+*/
+bool Concurrent_List_Contains(list_t*L, char* title, char* value);
