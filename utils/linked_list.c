95af3675d392eb020014e2be130b668f4341fd64
hoangson <lehoangsonsg@gmail.com>
2023-10-11 18:30:48 +1030

    feat: refactor linked list

---
+++ utils/linked_list.c
@@ -0,0 +1,98 @@
+#include "linked_list.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+void List_Init(linked_list_t* LL, char** pattern, char** title) {
+    LL->pattern_head = NULL;
+    LL->pattern_tail = NULL;
+    LL->head = NULL;
+    LL->tail = NULL;
+    pthread_mutex_init(&LL->lock, NULL);
+    LL->size = 0;
+    LL->pattern_count = 0;
+    LL->pattern = pattern;
+    LL->title = title;
+}
+
+int List_Insert(linked_list_t* LL, char* content) {
+    node_t* tmp;
+    int status = Node_Init(content, LL->pattern, &tmp);
+    // Halts if cannot create node
+    if (status != 0)
+        return status;
+    // Add new node to list
+    pthread_mutex_lock(&LL->lock);
+    LL->size += 1;
+    if (LL->head == NULL)
+        LL->head = tmp;
+    if (LL->tail == NULL)
+        LL->tail = tmp;
+    else {
+        LL->tail->book_next = tmp;
+        LL->tail = tmp;
+    }
+    // Add currrent node to list of search patterns if contains pattern
+    if (tmp->pattern_count != 0) {
+        LL->pattern_count += tmp->pattern_count;
+        if (LL->pattern_head == NULL)
+            LL->pattern_head = tmp;
+        if (LL->pattern_tail == NULL)
+            LL->pattern_tail = tmp;
+        else {
+            LL->pattern_tail->next_frequent_search = tmp;
+            LL->pattern_tail = tmp;
+        }
+    }
+    pthread_mutex_unlock(&LL->lock);
+    return 0;
+}
+
+bool List_Contains(linked_list_t* LL, char* content) {
+    pthread_mutex_lock(&LL->lock);
+    node_t* current_node = LL->head;
+    while (current_node != NULL) {
+        if ((strcmp(content, current_node->content) == 0)) {
+            pthread_mutex_unlock(&LL->lock);
+            return true;
+        }
+        current_node = current_node->book_next;
+    }
+    pthread_mutex_unlock(&LL->lock);
+    return false;
+}
+
+void List_Free(linked_list_t* LL) {
+    pthread_mutex_lock(&LL->lock);
+    node_t* current_node = LL->head;
+    while (current_node != NULL) {
+        node_t* new_node = current_node->book_next;
+        Node_Free(current_node);
+        free(current_node);
+        current_node = new_node;
+    }
+    LL->head = LL->tail = NULL;
+    LL->pattern_head = LL->pattern_tail = NULL;
+    pthread_mutex_unlock(&LL->lock);
+}
+
+void List_Write_Book(linked_list_t* L, int book_id) {
+    pthread_mutex_lock(&L->lock);
+    char file_name[SAVENAMEMAX];
+    if (book_id < 10)
+        sprintf(file_name, "book_0%d.txt", book_id);
+    else
+        sprintf(file_name, "book_%d.txt", book_id);
+    FILE* fp = fopen(file_name, "wb");
+    if (fp == NULL) {
+        printf("Cannot open file_name: %s\n", file_name);
+        pthread_mutex_unlock(&L->lock);
+        return;
+    }
+    node_t* current = L->head;
+    while (current != NULL) {
+        fputs(current->content, fp);
+        current = current->book_next;
+    }
+    fclose(fp);
+    pthread_mutex_unlock(&L->lock);
+}
---
+++ utils/linked_list.h
@@ -0,0 +1,108 @@
+#include <pthread.h>
+#include <stdbool.h>
+#include "node.h"
+#define SAVENAMEMAX 128  // Max size of filenames
+
+/*
+Internal data structure used in hash map 
+
+linked_list contains nodes of the same book title, arranged in by the order or 
+read. New node is added to the tail end of the list. 
+
+Fields: 
+    node_t* pattern_head;  // ptr to the head of the pattern search
+    node_t* pattern_tail;  // ptr to the tail of the pattern search
+    node_t* head;          // ptr to the head of the list
+    node_t* tail;          // ptr to the tail of the list
+    pthread_mutex_t lock;  // list lock
+    int size;              // list size
+    char** pattern;        // search pattern string
+    char** title;          // title
+    int pattern_count;     // pattern count
+*/
+typedef struct ___linked_list_t {
+    node_t* pattern_head;  // ptr to the head of the pattern search
+    node_t* pattern_tail;  // ptr to the tail of the pattern search
+    node_t* head;          // ptr to the head of the list
+    node_t* tail;          // ptr to the tail of the list
+    pthread_mutex_t lock;  // list lock
+    int size;              // list size
+    char** pattern;        // search pattern string
+    char** title;          // title
+    int pattern_count;     // pattern count
+} linked_list_t;
+
+/*
+Initialise linked list 
+
+Set:
+    pattern_head to NULL
+    pattern_tail to NULL 
+    head to NULL 
+    tail to NULL 
+    init lock 
+    size to 0 
+    pattern_count to 0 
+    title to title 
+    pattern to pattern 
+
+Arguments:
+LL - pointer ot linked list
+pattern - pointer to pattern string 
+title - pointer to title string 
+*/
+void List_Init(linked_list_t* LL, char** pattern, char** title);
+
+/*
+Insert new node to the list
+- Increment size 
+- Create new_node containing content 
+- If head is NULL, set head to new_node (When list is empty)
+- If tail is NULL, set tail to new_node (When list is empty)
+- If list is not empty, set new_node to book_next of tail 
+- If content has pattern:
+    - Increase pattern_count 
+    - If pattern_head and pattern_tail is NULL, set to new_node
+    - Otherwise set new_node to next_frequent_search of pattern_tail
+
+Arguments: 
+LL - pointer to the linked list 
+content - string book content 
+
+Returns:
+0 - if the operation was sucessful 
+1 - if the operation fails 
+*/
+int List_Insert(linked_list_t* LL, char* content);
+
+/*
+Check if list contains content
+
+Arguments: 
+LL - pointer to the linked list 
+content - string book content 
+
+Returns: 
+true - if the list contains any node with the same title 
+false - otherwise 
+*/
+bool List_Contains(linked_list_t* LL, char* content);
+
+/*
+Write List Content to text file
+
+Argument:
+LL - pointer to the linked list with content 
+book_id: name of book
+*/
+void List_Write_Book(linked_list_t* LL, int book_id);
+
+/*
+Free up memory of the linked list 
+
+Iteratively free the nodes in the list 
+
+Argument: 
+LL - pointer to the linked list to free memory of 
+*/
+void List_Free(linked_list_t* LL);
