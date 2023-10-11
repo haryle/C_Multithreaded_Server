3e7a5048e9d286419644bcf9e8fc0b9989308260
hoangson <lehoangsonsg@gmail.com>
2023-10-11 22:14:20 +1030

    feat: refactor map

--- utils/map.c
+++ utils/map.c
@@ -8,116 +8,16 @@
 #include <string.h>
 #define SEED 0x12345678
 
-int Node_Init(char* content, char** pattern, node_t** dst) {
-    *dst = (node_t*)malloc(sizeof(node_t));
-    if (*dst == NULL) {
-        perror("malloc");
-        return 1;
-    }
-    (*dst)->content = (char*)malloc(sizeof(char) * (strlen(content) + 1));
-    if ((*dst)->content == NULL) {
-        perror("malloc");
-        return 1;
-    }
-    (*dst)->next = NULL;
-    (*dst)->book_next = NULL;
-    (*dst)->next_frequent_search = NULL;
-    (*dst)->pattern_count = 0;
-    strcpy((*dst)->content, content);
-    (*dst)->pattern_count = count_occurence((*dst)->content, *pattern);
-    return 0;
-}
-
-void Node_Free(node_t* N) {
-    free(N->content);
-}
-
-void List_Init(linked_list_t* LL, char** pattern, char** title) {
-    LL->pattern_head = NULL;
-    LL->pattern_tail = NULL;
-    LL->head = NULL;
-    LL->tail = NULL;
-    pthread_mutex_init(&LL->lock, NULL);
-    LL->size = 0;
-    LL->pattern = pattern;
-    LL->title = title;
-    LL->pattern_count = 0;
-}
-
-int List_Insert(linked_list_t* LL, char* content) {
-    node_t* tmp;
-    int status = Node_Init(content, LL->pattern, &tmp);
-    // Add new node to list
-    pthread_mutex_lock(&LL->lock);
-    LL->size += 1;
-    if (LL->head == NULL)
-        LL->head = tmp;
-    if (LL->tail == NULL)
-        LL->tail = tmp;
-    else {
-        LL->tail->book_next = tmp;
-        LL->tail = tmp;
-    }
-    // Add currrent node to list of search patterns if contains pattern
-    if (tmp->pattern_count != 0) {
-        LL->pattern_count += tmp->pattern_count;
-        if (LL->pattern_head == NULL)
-            LL->pattern_head = tmp;
-        if (LL->pattern_tail == NULL)
-            LL->pattern_tail = tmp;
-        else {
-            LL->pattern_tail->next_frequent_search = tmp;
-            LL->pattern_tail = tmp;
-        }
-    }
-    pthread_mutex_unlock(&LL->lock);
-    return 0;
-}
-
-bool List_Contains(linked_list_t* LL, char* content) {
-    pthread_mutex_lock(&LL->lock);
-    node_t* current_node = LL->head;
-    while (current_node != NULL) {
-        if ((strcmp(content, current_node->content) == 0)) {
-            pthread_mutex_unlock(&LL->lock);
-            return true;
-        }
-        current_node = current_node->book_next;
-    }
-    pthread_mutex_unlock(&LL->lock);
-    return false;
-}
-
-void List_Free(linked_list_t* LL) {
-    pthread_mutex_lock(&LL->lock);
-    node_t* current_node = LL->head;
-    while (current_node != NULL) {
-        node_t* new_node = current_node->book_next;
-        Node_Destroy(current_node);
-        free(current_node);
-        current_node = new_node;
-    }
-    LL->head = LL->tail = NULL;
-    pthread_mutex_unlock(&LL->lock);
-}
-
-int hash(char* str) {
-    // One-byte-at-a-time hash based on Murmur's mix
-    // Source: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp
-    unsigned long h = (int)SEED;
-    for (; *str; ++str) {
-        h ^= *str;
-        h *= 0x5bd1e995;
-        h ^= h >> 15;
-    }
-    return h % CAPACITY;
-}
-
-void Map_Init(map_t* M, char* pattern) {
+void Map_Init(map_t* M, char** pattern) {
+    M->pattern = pattern;
     pthread_mutex_init(&M->lock, NULL);
     M->size = 0;
-    for (int i = 0; i < CAPACITY; i++)
-        List_Init(&M->lists[i], pattern);
+}
+
+linked_list_t* Map_Get(map_t* M, char* title) {
+    if (Map_Contains(M, title))
+        return &M->lists[hash(title)];
+    return NULL;
 }
 
 bool Map_Contains(map_t* M, char* title) {
@@ -137,84 +37,41 @@ bool Map_Contains(map_t* M, char* title) {
     return contains;
 }
 
-int Map_Insert(map_t* M, char* title, char* value) {
+int Map_Insert(map_t* M, char* title, char* content) {
     if (Map_Contains(M, title) == false) {
         if (M->size < CAPACITY) {
             // Add new key to list
             pthread_mutex_lock(&M->lock);
+            // Allocate new list:
+            linked_list_t* L = Map_Get(M, title);
+            L = (linked_list_t*)malloc(sizeof(linked_list_t));
+            if (L == NULL) {
+                pthread_mutex_unlock(&M->lock);
+                return -1;
+            }
+            // Add title to keys
             M->keys[M->size] = title;
             M->size++;
             pthread_mutex_unlock(&M->lock);
         }
     }
-    int index = hash(title);
-    int status = List_Insert(&M->lists[index], title, value);
+    int status = List_Insert(Map_Get(M, title), content);
     return status;
 }
 
+int hash(char* str) {
+    // One-byte-at-a-time hash based on Murmur's mix
+    // Source: https://github.com/aappleby/smhasher/blob/master/src/Hashes.cpp
+    unsigned long h = (int)SEED;
+    for (; *str; ++str) {
+        h ^= *str;
+        h *= 0x5bd1e995;
+        h ^= h >> 15;
+    }
+    return h % CAPACITY;
+}
+
 void Map_Free(map_t* M) {
-    for (int i = 0; i < CAPACITY; i++)
-        List_Free(&M->lists[i]);
-}
-
-linked_list_t* Map_Get(map_t* M, char* title) {
-    return &M->lists[hash(title)];
-}
-
-node_t* List_Head(linked_list_t* L) {
-    pthread_mutex_lock(&L->lock);
-    node_t* head = L->head;
-    pthread_mutex_unlock(&L->lock);
-    return head;
-}
-
-node_t* List_Tail(linked_list_t* L) {
-    pthread_mutex_lock(&L->lock);
-    node_t* tail = L->tail;
-    pthread_mutex_unlock(&L->lock);
-    return tail;
-}
-
-void List_Write_Book(linked_list_t* L, int book_id) {
-    pthread_mutex_lock(&L->lock);
-    char file_name[128];
-    if (book_id < 10)
-        sprintf(file_name, "book_0%d.txt", book_id);
-    else
-        sprintf(file_name, "book_%d.txt", book_id);
-    FILE* fp = fopen(file_name, "wb");
-    if (fp == NULL) {
-        printf("Cannot open file_name: %s\n", file_name);
-        pthread_mutex_unlock(&L->lock);
-        return;
-    }
-    node_t* current = L->head;
-    while (current != NULL) {
-        fputs(current->value, fp);
-        current = current->book_next;
-    }
-    fclose(fp);
-    pthread_mutex_unlock(&L->lock);
-}
-
-int count_occurence(char* string, char* pattern) {
-    int result = 0;
-    char* tmp = (char*)malloc(sizeof(char) * (strlen(string) + 1));
-    if (tmp == NULL) {
-        perror("malloc");
-        return -1;
-    }
-    char* substr = strstr(string, pattern);
-    while (substr != NULL) {
-        result++;
-        if (strlen(substr) == strlen(pattern))
-            break;
-        char* src = &substr[strlen(pattern) + 1];
-        int size = strlen(substr + strlen(pattern) + 1);
-        memmove(tmp, src, size + 1);
-
-        substr = strstr(tmp, pattern);
-    }
-    free(tmp);
-    return result;
+    for (int i = 0; i < M->size; i++)
+        List_Free(Map_Get(M, M->keys[i]));
 }
--- utils/map.h
+++ utils/map.h
@@ -1,122 +1,42 @@
+#include <linked_list.h>
 #include <pthread.h>
 #include <stdbool.h>
-
 #ifndef CAPACITY
     #define CAPACITY 500000
 #endif
 
 /*
-Node data structure.
-
-Fields:
-    char* content;                // Line Content
-    int pattern_count;            // Number of pattern occurences
-    struct ___node_t* next;       // Link to the next element in the list
-    struct ___node_t* book_next;  // Link to the next item in the same book
-    struct ___node_t*
-        next_frequent_search;  // Link to the next item containing search pattern
-*/
-typedef struct ___node_t {
-    char* content;                // Line Content
-    int pattern_count;            // Number of pattern occurences
-    struct ___node_t* next;       // Link to the next element in the list
-    struct ___node_t* book_next;  // Link to the next item in the same book
-    struct ___node_t*
-        next_frequent_search;  // Link to the next item containing search pattern
-} node_t;
-
-int Node_Create(char* content, char** pattern, node_t** dst);
-
-void Node_Free(node_t* N)
-
-/*
-Internal data structure used in hash map 
-
-linked_list contains nodes of the same book title, arranged in by the order or 
-read. New node is added to the tail end of the list. 
+Thread-safe hashmap data structure 
 
 Fields: 
-    node_t* pattern_head;  // ptr to the head of the pattern search
-    node_t* pattern_tail;  // ptr to the tail of the pattern search
-    node_t* head;          // ptr to the head of the list
-    node_t* tail;          // ptr to the tail of the list
-    pthread_mutex_t lock;  // list lock
-    int size;              // list size
-    char** pattern;        // search pattern string
-    char** title;          // title
-    int pattern_count;     // pattern count
+lists[CAPACITY] - internal linked list 
 */
-typedef struct ___linked_list_t {
-    node_t* pattern_head;  // ptr to the head of the pattern search
-    node_t* pattern_tail;  // ptr to the tail of the pattern search
-    node_t* head;          // ptr to the head of the list
-    node_t* tail;          // ptr to the tail of the list
-    pthread_mutex_t lock;  // list lock
-    int size;              // list size
-    char** pattern;        // search pattern string
-    char** title;          // title
-    int pattern_count;     // pattern count
-} linked_list_t;
+typedef struct ___map_t {
+    char** pattern;                  // Search pattern
+    int size;                        // Map size
+    pthread_mutex_t lock;            // Lock
+    char* keys[CAPACITY];            // Inserted keys
+    linked_list_t* lists[CAPACITY];  // List pointers
+} map_t;
 
-/* 
-Get the head node of the list
+/* Initialise the map 
+
+Set:
+    pattern to pattern
+    initialise lock
+    size to 0 
 */
-node_t* List_Head(linked_list_t* L);
+void Map_Init(map_t* M, char** pattern);
 
 /*
-Get the tail node of the list 
+Get LinkedList corresponding to title 
+
+Return
+Pointer to linked list or NULL if map does not 
+contain title 
 */
-node_t* List_Tail(linked_list_t* L);
+linked_list_t* Map_Get(map_t* M, char* title);
 
-/*
-Initialise linked list 
-*/
-void List_Init(linked_list_t* LL, char** pattern, char** title);
-
-/*
-Insert new node to the list
-
-Arguments: 
-LL - pointer to the linked list 
-title - string book title 
-value - string book content 
-
-Returns:
-0 - if the operation was sucessful 
-1 - if the operation fails 
-*/
-int List_Insert(linked_list_t* LL, char* content);
-
-/*
-Check if list contains a title
-
-Arguments: 
-LL - pointer to the linked list 
-title - string book title to search 
-value - string book content 
-
-Returns: 
-true - if the list contains any node with the same title 
-false - otherwise 
-*/
-bool List_Contains(linked_list_t* LL, char* content);
-
-/*
-Free up memory of the linked list 
-
-Argument: 
-LL - pointer to the linked list to free memory of 
-*/
-void List_Free(linked_list_t* LL);
-
-/*
-Write List Content to text file
-
-Argument:
-LL - pointer to the linked list with content 
-book_id: name of book
-*/
-void List_Write_Book(linked_list_t* LL, int book_id);
 
 /*
 Create a hash of the input string 
@@ -127,25 +47,9 @@ str - string to create a hash out of
 int hash(char* str);
 
 /*
-Thread-safe hashmap data structure 
-
-Fields: 
-lists[CAPACITY] - internal linked list 
-*/
-typedef struct ___map_t {
-    pthread_mutex_t lock;
-    char* keys[CAPACITY];
-    int size;
-    linked_list_t lists[CAPACITY];
-} map_t;
-
-/*
 Check if map contains title
 */
 bool Map_Contains(map_t* M, char* title);
-
-/* Initialise the map */
-void Map_Init(map_t* M, char* pattern);
 
 /*
 Insert new node to the map
@@ -164,9 +68,3 @@ int Map_Insert(map_t* M, char* title, char* value);
 /* Free map memory */
 void Map_Free(map_t* M);
 
-/*
-Get LinkedList corresponding to title 
-*/
-linked_list_t* Map_Get(map_t* M, char* title);
-
-int count_occurence(char* string, char* pattern);
